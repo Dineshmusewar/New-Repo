06f9c4a7c490f0fe0ef71e2ce2127e84e975a624
Dineshmusewar <153366647+Dineshmusewar@users.noreply.github.com>
2025-08-18 14:38:00 +0530

    variable a added on line 5 in first.java

--- first.java
+++ first.java
@@ -2,5 +2,6 @@
 public class first{
     public static void main(String args[]){
         System.out.println("This is my first Java class");
+        int a=1; // initializing a local variable a;
     }
 }
